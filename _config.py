716bb40e830409af113be016ebb80aef79347af7
Andrey Petrov <andrey.petrov@shazow.net>
2011-07-13 11:08:11 -0700

    site.url

--- _config.py
+++ _config.py
@@ -1,4 +1,1 @@
-# This is a barren blogofile config file.
-# See docs at http://www.blogofile.com/documentation
-# for config options
-# or run 'blogofile help init' to see more complete templates
+site.url = "http://localhost:8080/"
