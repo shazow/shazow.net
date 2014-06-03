fddef78e352e164449dd3d7ac9e638f6962193d9
Andrey Petrov <andrey.petrov@shazow.net>
2014-06-02 19:13:45 -0700

    Use bower and compass.

--- Makefile
+++ Makefile
@@ -28,3 +28,18 @@ pull:
 
 clean:
 	rm -rf ./build
+
+
+## CSS:
+
+.env/bower: bower.json
+	bower update
+
+css_watch:
+	compass watch ./compass
+
+css:
+	compass compile ./compass
+
+css_production:
+	compass compile ./compass --output-style compressed --force
---
+++ compass/config.rb
@@ -0,0 +1,16 @@
+http_path = "/static"
+project_path = "_static/static"
+css_dir = "css"
+sass_dir = "../../compass/sass"
+images_dir = "images"
+javascripts_dir = "js"
+extensions_dir = ".env/bower"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
