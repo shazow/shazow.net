0b44622aa76b53ce158662309c5c445ed79742a1
Andrey Petrov <andrey.petrov@shazow.net>
2014-06-03 17:27:06 -0700

    Use normalize reset.

--- compass/config.rb
+++ compass/config.rb
@@ -1,10 +1,11 @@
+require "zurb-foundation"
+
 http_path = "/static"
-project_path = "_static/static"
-css_dir = "css"
-sass_dir = "../../compass/sass"
-images_dir = "images"
-javascripts_dir = "js"
-extensions_dir = ".env/bower"
+project_path = "_static/"
+css_dir = "static/css"
+sass_dir = "../compass/sass"
+images_dir = "static/images"
+javascripts_dir = "static/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :expanded
@@ -14,3 +15,9 @@ relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
+
+# Disable deprecation warnings (Foundation 5 bug)
+sass_options = {:quiet => true}
+
+# Extra bower components (relative to project_path, seems like)
+#add_import_path "../.env/bower/foundation/scss"
