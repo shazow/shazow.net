05dd61d679ff0adcfd2dc0fa5ce388a8f72787a3
Andrey Petrov <andrey.petrov@shazow.net>
2025-08-08 19:40:00 -0700

    assets/js/post.js: More sophisticated sidenote restacking

--- assets/js/post.js
+++ assets/js/post.js
@@ -14,20 +14,39 @@
         sidenotes.className = "sidenotes"
         post.appendChild(sidenotes);
 
-        let lastOffset = 0;
+        // Track the sups in the respective idx so we can restack against them later
+        const sups = [];
+
         post.querySelectorAll(".footnotes li").forEach((footnote) => {
             // <a href="#fnref:1" class="footnote-backref" role="doc-backlink">↩︎</a>
-            const backref = footnote.getElementsByClassName('footnote-backref')[0].getAttribute("href");
+            const backref = footnote.querySelector('.footnote-backref').getAttribute("href");
 
             // <sup id="fnref:1"><a ...>1</a></sup>
             const sup = document.getElementById(backref.slice(1));
+            sups.push(sup);
 
-            // Stack notes vertically as long as the top position is at least the position of the sup link.
             const sidenote = footnote.cloneNode(true);
             sidenotes.appendChild(sidenote);
-            const top = Math.max(sup.offsetTop, lastOffset);
-            sidenote.style.top = top + "px";
-            lastOffset = top + sidenote.getBoundingClientRect().height;
+        });
+
+        if (!sidenotes.hasChildNodes()) return;
+
+        function restack() {
+            // Stack notes vertically as long as the top position is at least the position of the sup link.
+            let lastOffset = 0;
+            sidenotes.childNodes.forEach((sidenote, idx) => {
+                const top = Math.max(sups[idx].offsetTop, lastOffset);
+                sidenote.style.top = top + "px";
+                lastOffset = top + sidenote.getBoundingClientRect().height;
+            });
+        };
+        restack();
+
+        // Apply restacking on resize, once every 200ms
+        let debounce;
+        window.addEventListener('resize', () => {
+            clearTimeout(debounce);
+            debounce = setTimeout(restack, 200);
         });
     })
 })()
